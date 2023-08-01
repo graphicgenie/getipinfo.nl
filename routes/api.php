c95ac6a00ebc988b29237b9000ca09cbe87afac8
Ronald Gerssen <ronald@graphicgenie.nl>
2023-08-01 11:20:46 +0200

    Ratelimit 30 per minute

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IPController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('ip', [\App\Http\Controllers\IPController::class, 'index']);
+Route::get('ip', [IPController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
